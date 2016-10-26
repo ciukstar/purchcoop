ca4f3e81dd92b2f1c21c3b7dea92bd5f6dee8851
Sergiu Starciuc <ciukstar@yahoo.com>
2016-10-26 17:59:37 +0300

    Some changes

---
+++ src/main/scala/domain/Order.scala
@@ -0,0 +1,19 @@
+package domain {
+
+  import java.time._
+
+  class Order (
+    val id: Long,
+    val customerId: Long,
+    val purchaseId: Long,
+    val quantity: Int,
+    val orderDate: LocalDateTime
+  ) {
+
+    def this(customer: User, purchase: Purchase, quantity: Int) =
+      this(-1, customer.id, purchase.id, quantity, LocalDateTime.now())
+
+    def this(customer: User, purchase: Purchase, quantity: Int, orderDate: LocalDateTime)
+      = this(-1, customer.id, purchase.id, quantity, orderDate)
+  }
+}
